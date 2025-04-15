25ca5771a741ceabbb905e993f9254321ab5db47
LuizCarlosSB <147108834+LuizCarlosSB@users.noreply.github.com>
2025-04-15 16:26:59 +0000

    teste

--- CryptLock/App.js
+++ CryptLock/App.js
@@ -6,7 +6,7 @@ const Drawer = createDrawerNavigator();
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Home Screeeen</Text>
     </View>
   );
 }
